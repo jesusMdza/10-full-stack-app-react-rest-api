74b101a06877ccee24813c38e1f1230595b1d8c4
Jesus Mendoza <jesuszmendoza14@gmail.com>
2020-03-14 17:27:30 -0500

    add funcs
    - retrieve data when mounted
    - retrieve Course component data

--- client/src/components/Courses.js
+++ client/src/components/Courses.js
@@ -1,36 +1,51 @@
 import React from 'react';
+import Course from './Course';
 
 class Courses extends React.Component {
 
   state = {
-    courses: this.props.context.actions.getCourses()
+    courses: []
+  }
+
+  // Invokes "getCourses" and sets "courses" state to data received back
+  componentDidMount() {
+    const { context } = this.props;
+    context.actions.getCourses()
+      .then(data => {
+        this.setState({courses: data});
+      });
+  }
+
+  // Returns list of courses
+  returnCourses = () => {
+    const { courses } = this.state;
+    const courseData = courses.map(course => 
+      <Course 
+        key={course.id} 
+        id={course.id} 
+        title={course.title} />
+    );
+
+    return courseData;
   }
 
   render() {
     const { courses } = this.state;
-    const { id, title } = this.state.courses;
 
     return(
       <div className="bounds">
-        {/* { 
+        { 
           courses ? 
-          courses.map(course => {
-            return <div className="grid-33">
-              <a className="course--module course--link" href={`/courses/${id}`}>
-                <h4 className="course--label">Course</h4>
-                <h3 className="course--title">{title}</h3>
-              </a>
-            </div>         
-          })
+          this.returnCourses()
           : 
           null 
-        } */}
-        <a className="course--module course--add--module" href="create-course.html">
-          <h3 className="course--add--title"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
-              viewBox="0 0 13 13" className="add">
-              <polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 "></polygon>
-            </svg>New Course</h3>
-        </a>
+        }
+        <div class="grid-33"><a class="course--module course--add--module" href="create-course.html">
+            <h3 class="course--add--title"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+                viewBox="0 0 13 13" class="add">
+                <polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 "></polygon>
+              </svg>New Course</h3>
+          </a></div>
       </div>
     );
   }
