ac8b76b8966ff8fbb32255cf210f40f63f519644
Jesus Mendoza <jesuszmendoza14@gmail.com>
2020-03-17 21:36:24 -0500

    adjust jsx markup

--- client/src/components/Courses.js
+++ client/src/components/Courses.js
@@ -41,12 +41,15 @@ export default class Courses extends React.Component {
           : 
           null 
         }
-        <div className="grid-33"><a className="course--module course--add--module" href="create-course.html">
-            <h3 className="course--add--title"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+        <div className="grid-33">
+          <a className="course--module course--add--module" href="/courses/create">
+            <h3 className="course--add--title">
+              <svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
                 viewBox="0 0 13 13" className="add">
                 <polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 "></polygon>
               </svg>New Course</h3>
-          </a></div>
+          </a>
+        </div>
       </div>
     );
   }
