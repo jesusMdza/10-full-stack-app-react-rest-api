8a0d9775a7e429a522bcf4e959c075dfe99e141c
Jesus Mendoza <jesuszmendoza14@gmail.com>
2020-03-24 20:02:13 -0500

    adjust jsx markup

--- client/src/components/UpdateCourse.js
+++ client/src/components/UpdateCourse.js
@@ -26,13 +26,19 @@ class UpdateCourse extends React.Component {
             <div className="grid-66">
               <div className="course--header">
                 <h4 className="course--label">Course</h4>
-                <div><input id="title" name="title" type="text" className="input-title course--title--input" placeholder="Course title..."
-                    value={ title } /></div>
+                <div>
+                  <input 
+                    id="title" name="title" type="text" className="input-title course--title--input" placeholder="Course title..."
+                    value={ title } 
+                  />
+                </div>
                 <p>By Joe Smith</p>
               </div>
               <div className="course--description">
                 <div>
-                  <textarea id="description" name="description" className="" placeholder="Course description..." value={ description }></textarea>
+                  <textarea 
+                    id="description" name="description" className="" placeholder="Course description..." value={ description }>
+                  </textarea>
                 </div>
               </div>
             </div>
@@ -41,13 +47,20 @@ class UpdateCourse extends React.Component {
                 <ul className="course--stats--list">
                   <li className="course--stats--list--item">
                     <h4>Estimated Time</h4>
-                    <div><input id="estimatedTime" name="estimatedTime" type="text" className="course--time--input"
-                        placeholder="Hours" value={ estimatedTime } /></div>
+                    <div>
+                      <input 
+                        id="estimatedTime" name="estimatedTime" type="text" className="course--time--input"
+                        placeholder="Hours" value={ estimatedTime } 
+                      />
+                    </div>
                   </li>
                   <li className="course--stats--list--item">
                     <h4>Materials Needed</h4>
                     <div>
-                      <textarea id="materialsNeeded" name="materialsNeeded" className="" placeholder="List materials..." value={ materialsNeeded }></textarea>
+                      <textarea 
+                        id="materialsNeeded" name="materialsNeeded" className="" placeholder="List materials..." 
+                        value={ materialsNeeded }>
+                      </textarea>
                     </div>
                   </li>
                 </ul>
