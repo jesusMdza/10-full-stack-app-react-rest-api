810ffed54644903a1051192aa366f1d57f3e457a
Jesus Mendoza <jesuszmendoza14@gmail.com>
2020-03-27 22:44:57 -0500

    add Forbidden component

---
+++ client/src/components/Forbidden.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Forbidden = () => {
+  return(
+    <div className="bounds">
+      <h1>Forbidden</h1>
+      <p>You are not authorized to update this course.</p>
+    </div>
+  );
+}
+
+export default Forbidden;
