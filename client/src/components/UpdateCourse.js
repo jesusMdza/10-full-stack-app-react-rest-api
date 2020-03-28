02604677270282cd0d4e1e9c6cb5011ece4a7f38
Jesus Mendoza <jesuszmendoza14@gmail.com>
2020-03-27 22:46:12 -0500

    changes
    -update state
    -update conditionals

--- client/src/components/UpdateCourse.js
+++ client/src/components/UpdateCourse.js
@@ -3,33 +3,87 @@ import React from 'react';
 class UpdateCourse extends React.Component {
 
   state = {
-    course: {}
+    title: "",
+    description: "",
+    estimatedTime: "",
+    materialsNeeded: "",
+    userId: "",
+    errors: []
+  }
+
+  change = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+
+    this.setState({
+      [name]: value
+    });
   }
 
   componentDidMount() {
     const { context, match } = this.props;
     context.actions.getCourses(match.params.id)
     .then(data => {
-      this.setState({course: data});
+      if (data === null) {
+        this.props.history.push("/notfound");
+      } else {
+        this.setState({
+          title: data.title,
+          description: data.description,
+          estimatedTime: data.estimatedTime,
+          materialsNeeded: data.materialsNeeded,
+          userId: data.userId
+        });
+      }
+    })
+    .catch(err => {
+      this.props.history.push("/error");
     });
   }
 
-  render(){
-    const { course } = this.state;
-    const { title, description, estimatedTime, materialsNeeded } = course;
+  submit = (e) => {
+    e.preventDefault();
+    const { title, description, estimatedTime, materialsNeeded, userId } = this.state;
+    const { context } = this.props;
+
+    const body = {
+      title,
+      description,
+      estimatedTime,
+      materialsNeeded,
+      userId
+    };
+
+    if (context.authenticatedUser.id === userId) {
+      context.actions.postCourse(body)
+      .then(errors => {
+        if (errors) {
+          this.setState({ errors: errors.error });
+        } else {
+          context.actions.putCourse(body);
+        }
+      })
+      .catch(err => this.props.history.push("/error"));
+    } else {
+      this.props.history.push("/forbidden");
+    }
+  } 
+
+  render() {
+    const { title, description, estimatedTime, materialsNeeded } = this.state;
 
     return(
       <div className="bounds course--detail">
         <h1>Update Course</h1>
         <div>
-          <form>
+          <form onSubmit={ (e) => this.submit(e) }>
             <div className="grid-66">
               <div className="course--header">
                 <h4 className="course--label">Course</h4>
                 <div>
                   <input 
                     id="title" name="title" type="text" className="input-title course--title--input" placeholder="Course title..."
-                    value={ title } 
+                    value={ title } onChange={ (e) => this.change(e) }
                   />
                 </div>
                 <p>By Joe Smith</p>
@@ -37,7 +91,7 @@ class UpdateCourse extends React.Component {
               <div className="course--description">
                 <div>
                   <textarea 
-                    id="description" name="description" className="" placeholder="Course description..." value={ description }>
+                    id="description" name="description" className="" placeholder="Course description..." value={ description } onChange={ (e) => this.change(e) }>
                   </textarea>
                 </div>
               </div>
@@ -50,7 +104,7 @@ class UpdateCourse extends React.Component {
                     <div>
                       <input 
                         id="estimatedTime" name="estimatedTime" type="text" className="course--time--input"
-                        placeholder="Hours" value={ estimatedTime } 
+                        placeholder="Hours" value={ estimatedTime } onChange={ (e) => this.change(e) }
                       />
                     </div>
                   </li>
@@ -59,14 +113,14 @@ class UpdateCourse extends React.Component {
                     <div>
                       <textarea 
                         id="materialsNeeded" name="materialsNeeded" className="" placeholder="List materials..." 
-                        value={ materialsNeeded }>
+                        value={ materialsNeeded } onChange={ (e) => this.change(e) }>
                       </textarea>
                     </div>
                   </li>
                 </ul>
               </div>
             </div>
-            <div className="grid-100 pad-bottom"><button className="button" type="submit">Update Course</button><button className="button button-secondary" onclick="event.preventDefault(); location.href='course-detail.html';">Cancel</button></div>
+            <div className="grid-100 pad-bottom"><button className="button" type="submit">Update Course</button><button className="button button-secondary" onClick={(e) => {e.preventDefault(); this.props.history.push('/')}}>Cancel</button></div>
           </form>
         </div>
       </div>
