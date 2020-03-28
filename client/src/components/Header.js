f8e75068565f9070ab6478b98a3fd6f8e2147d1a
Jesus Mendoza <jesuszmendoza14@gmail.com>
2020-03-27 22:45:18 -0500

    add href

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,18 +1,19 @@
 import React from 'react';
 
 const Header = (props) => {
-
   const { context } = props;
 
   return (
     <div className="header">
       <div className="bounds">
-        <h1 className="header--logo">Courses</h1>
+        <a href="/">
+          <h1 className="header--logo">Courses</h1>
+        </a>
         <nav>
           {
             context.authenticatedUser !== null ?
             <>
-              <span>Welcome { context.authenticatedUser.firstName }!</span>
+              <span>Welcome { context.authenticatedUser.firstName.charAt(0).toUpperCase() + context.authenticatedUser.firstName.slice(1) }!</span>
               <a className="signout" href="/signout">Sign Out</a>
             </>
             :
