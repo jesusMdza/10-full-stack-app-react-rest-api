8dd774cfd967e12f0fa3f2abe081ae33be3d18da
Jesus Mendoza <jesuszmendoza14@gmail.com>
2020-04-18 20:36:53 -0500

    Modify class names

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -6,15 +6,13 @@ const Header = (props) => {
   return (
     <div className="header">
       <div className="bounds">
-        <a href="/">
-          <h1 className="header--logo">Courses</h1>
-        </a>
+        <a href="/">Courses</a>
         <nav>
           {
             context.authenticatedUser !== null ?
             <>
-              <span>Welcome { context.authenticatedUser.firstName.charAt(0).toUpperCase() + context.authenticatedUser.firstName.slice(1) }!</span>
-              <a className="signout" href="/signout">Sign Out</a>
+              <span>Welcome { context.authenticatedUser.firstName.charAt(0).toUpperCase() + context.authenticatedUser.firstName.slice(1) }</span>
+              <a href="/signout">Sign Out</a>
             </>
             :
             <>
