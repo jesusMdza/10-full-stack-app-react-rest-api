d1246addd53f2c4ea351832b07685d0b22f150ed
Jesus Mendoza <jesuszmendoza14@gmail.com>
2020-05-05 15:52:58 -0500

    update href

--- client/src/components/CourseDetail.js
+++ client/src/components/CourseDetail.js
@@ -7,8 +7,10 @@ export default class CourseDetail extends React.Component {
     course: {},
   }
 
+  // retrieves course data by id parameter
   componentDidMount() {
     const { context, match } = this.props;
+    
     context.actions.getCourses(match.params.id)
     .then(data => {
       if (data === null) {
@@ -22,23 +24,6 @@ export default class CourseDetail extends React.Component {
     .catch(err => {
       this.props.history.push("/error");
     }); 
-  }
-
-  // Delete's course if authenticated user owns course
-  isOwnerDeleteCourse = (id) => {
-    const { context } = this.props;
-    const { course } = this.state;
-    const { owner } = course;
-
-    if (owner.id === context.authenticatedUser.id) {
-      context.actions.deleteCourse(id)
-        .then(data => this.props.history.push("/"))
-        .catch(err => {
-          this.props.history.push("/error");
-        });
-    } else {
-      this.props.history.push("/forbidden");
-    }
   }
 
   render(){
@@ -76,21 +61,21 @@ export default class CourseDetail extends React.Component {
                 {
                   authUserId && ownerId === authUserId ?
                     <>
-                    <a className="button update-button" href={`/courses/${ id }/update`}>
-                      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round" className="feather feather-edit">
-                        <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path>
-                        <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path>
-                      </svg>
-                      <span>Edit Course</span>
-                    </a>
-                    <button className="button delete-button" onClick={ () => this.isOwnerDeleteCourse( id )}>
-                      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round" className="feather feather-trash-2">
-                        <polyline points="3 6 5 6 21 6"></polyline>
-                        <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
-                        <line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line>
-                      </svg>
-                      <span>Delete Course</span>
-                    </button>  
+                      <a className="button update-button" href={`/courses/${ id }/update`}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round" className="feather feather-edit">
+                          <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path>
+                          <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path>
+                        </svg>
+                        <span>Edit Course</span>
+                      </a>
+                      <a className="button delete-button" href={`/courses/${ id }/delete`}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round" className="feather feather-trash-2">
+                          <polyline points="3 6 5 6 21 6"></polyline>
+                          <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
+                          <line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line>
+                        </svg>
+                        <span>Delete Course</span>
+                      </a>  
                     </> 
                   :
                   null               
