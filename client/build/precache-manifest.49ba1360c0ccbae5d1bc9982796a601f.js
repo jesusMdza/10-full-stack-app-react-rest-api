7687fc77422299c6671969ff68a05b3285118c78
Jesus Mendoza <jesuszmendoza14@gmail.com>
2020-05-06 15:27:47 -0500

    add build file

---
+++ client/build/precache-manifest.49ba1360c0ccbae5d1bc9982796a601f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53e381ff025cf8804b8e104c0539f806",
+    "url": "/index.html"
+  },
+  {
+    "revision": "884dbf98e1f0a98a1876",
+    "url": "/static/css/main.96fc8fa0.chunk.css"
+  },
+  {
+    "revision": "06f017b1cd0c2defb38b",
+    "url": "/static/js/2.271b1d9a.chunk.js"
+  },
+  {
+    "revision": "ef82bb1f7813d0777b6cce74176089ce",
+    "url": "/static/js/2.271b1d9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "884dbf98e1f0a98a1876",
+    "url": "/static/js/main.ecb2711f.chunk.js"
+  },
+  {
+    "revision": "08a01cac7a9424eef63f",
+    "url": "/static/js/runtime-main.ef76e641.js"
+  }
+]);
